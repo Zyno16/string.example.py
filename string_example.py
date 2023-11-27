1ff469702de01637b34361a1d889929c1bebde96
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-27 19:12:03 +0000

    Add files via upload

---
+++ string_example.py
@@ -0,0 +1,21 @@
+str1 ="\"hello world\""
+print(str1)
+str2()"welcome to algeria\rhello") 
+print(str)  
+#will print hellome to algeria this mean will write over
+str3 ="\a"
+print(a)
+#this make sound 
+str4="hello\ba"
+print(str4)
+#will delete o from hello and replace it with a 
+# #will priny hella
+str5="hello\b\ba"
+print(str5) 
+#will delete l from hello and replace it with a 
+# #will priny helao 
+str6 = "hello\tWorld"
+print(str6)
+#\t is like tab
+
+    
